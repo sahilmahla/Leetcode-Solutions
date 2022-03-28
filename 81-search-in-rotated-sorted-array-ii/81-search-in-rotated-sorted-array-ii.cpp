d0d0c508aa85e1dbf7741fc21a3221ae591d9e12
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2022-03-28 10:55:29 +0530

    Time: 3 ms (95.97%), Space: 13.8 MB (90.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        for(auto it:nums){
-            if(it == target) return true;
+    bool help(vector<int>& nums,int l,int h,int target){
+        if(l>h){
+            return false;
         }
-        return false;
+        
+        int mid = l+(h-l)/2;
+        
+        if(nums[mid] == target || nums[l] == target || nums[h] == target){
+            return true;
+        }
+        bool a = false,b = false,c = false,d = false;
+        if(nums[mid]<=nums[h]){
+            if(target>nums[mid] && target<nums[h]){
+                a =  help(nums,mid+1,h,target);
+            }else{
+                b = help(nums,l,mid-1,target);
+            }
+        }
+        
+        if(nums[l]<=nums[mid]){
+            if(target > nums[l] && target < nums[mid]){
+                c = help(nums,l,mid-1,target);
+            }else{
+                d = help(nums,mid+1,h,target);
+            }
+        }
+        
+        return a||b||c||d;
+    }
+    
+    
+    bool search(vector<int>& nums, int target) {
+        return help(nums,0,nums.size()-1,target);
+        
     }
 };
