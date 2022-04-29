6c520da5154e2e7b94a53a16e238f15ef2055e28
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2022-04-29 15:11:53 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    struct Node* p1 = head;
+    struct Node* p2 = head;
+    int len = 0;
+    while( p1 && p1->next ){
+        p1 = p1->next->next;   
+        p2 = p2->next;
+        if(p1 == p2){
+            len = 1;
+            break;
+        }
+    }
+    
+    if(len == 0) return 0;
+    p2 = p2->next;
+    while(p1 != p2){
+        len++;
+        p2 = p2->next;
+    }
+    return len;
+}
